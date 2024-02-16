c7a3cebd8a02db66c137e231f6ca6a1673f7ce6b
joseph okwudi <josephflo55@gmail.com>
2024-02-16 11:14:06 -0700

    OIDC first commit

---
+++ environments/s3/backend.tf
@@ -0,0 +1,24 @@
+module "s3" {
+  source = "git@github.com:joflow05/s3-module.git//s3-module?ref=v1.0.0"
+}
+
+#Backend configuration
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "class400kops"
+    region  = "us-east-1"
+    key     = "s3/terraform.tfstate"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
---
+++ environments/vpc/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket  = "class400kopk"
+    region  = "us-east-1"
+    key     = "oidc/terraform.tfstate"
+    encrypt = true
+  }
+}
